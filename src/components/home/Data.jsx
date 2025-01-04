1c1f9450b4ecdfba243203043b1a15d07dcfc483
Vrajal <vrajal99@gmail.com>
2025-01-03 16:23:18 -0800

    feat:added contact component with email js library;to-do:check css of the contact section to fit in my screen; bug:no space between cal-icon and date in qualification.

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -60,6 +60,7 @@ const Data = () => {
         I'm software Engineer based in San Francisco, and I'm very passionate
         and dedicated to my work.
       </p>
+
       <a href="#contact" className="button button--flex">
         Say Hello!
         <svg
