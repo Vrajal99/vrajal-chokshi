0b045b97a75511a592d49c29371a6b5e77e1e206
Vrajal <vrajal99@gmail.com>
2025-01-05 14:51:36 -0800

    feat:added work experience/portfolio components

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
+import Work from "./components/work/Work";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Testimonials />
         <Contact />
       </main>
