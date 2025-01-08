83071b7b72f9dde2b056ac1a0a26ab6ab2100839
Vrajal <vrajal99@gmail.com>
2025-01-08 10:57:41 -0800

    update: modified the services section write-up and minor bug fixes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,7 +16,7 @@ const Services = () => {
         <div className="services__content">
           <i className="uil uil-web-grid services__icon"></i>
           <h3 className="services__title">
-            Software <br /> Development
+            Software <br /> Development - Freelancing
           </h3>
 
           <span className="services__button" onClick={() => toggleTab(1)}>
@@ -38,41 +38,43 @@ const Services = () => {
 
               <h3 className="services__modal-title">Software Development</h3>
               <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
+                Freelance with more than 3 years of professional experience.
+                Engineering high-quality and impactful software programs.
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop the web solutions.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I create ux element interactions.
+                    I develop the code solutions.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I position your company brand.
+                    End-to-end REST API development.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    Design and mockups of products for companies.
+                    I design and create optimized database architectutres.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    R & D of company stratgies through software.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Engineer and deploy back-end for business requirements.
                   </p>
                 </li>
               </ul>
@@ -106,15 +108,15 @@ const Services = () => {
                 E-commerce Web Solutions
               </h3>
               <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
+                Successfully built & delivered 3+ web solutions. Accelerating
+                businesses, using enterprise-level technlogies.
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop the web solutions.
+                    I craft solutions for taking business online.
                   </p>
                 </li>
 
@@ -126,21 +128,22 @@ const Services = () => {
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I create ux element interactions.
+                    I create dashboards, admin-panels and stock management and
+                    listing systems.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I position your company brand.
+                    I position your company's IT.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    Design and mockups of products for companies.
+                    Detailed anaylsis and web representation for companies.
                   </p>
                 </li>
               </ul>
@@ -172,41 +175,44 @@ const Services = () => {
               ></i>
               <h3 className="services__modal-title">Data & Cloud Solutions</h3>
               <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
+                Expertise in cloud platorms and data eng. .
+                <br /> Providing quality cloud adoption advise & strategies
+                backed by data.
               </p>
 
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I develop the web solutions.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I create ux element interactions.
+                    I leverage cloud servides like AWS and Azure.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    I position your company brand.
+                    Perform Data related operation on large-scale.
                   </p>
                 </li>
 
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
-                    Design and mockups of products for companies.
+                    I create data dashboards and cloud monitor interactions.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I bring your data into action for your company.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Cloud migration assitance and adoption.
                   </p>
                 </li>
               </ul>
